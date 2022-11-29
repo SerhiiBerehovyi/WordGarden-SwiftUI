40ae04777c1a91cd46b9031bde8d4198521abcf7
serhii.berehovyi <serhii.berehovyi@gmail.com>
2022-11-29 15:33:54 +0100

    Initial Commit

---
+++ WordGarden-SwiftUI/WordGarden_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGarden_SwiftUIApp.swift
+//  WordGarden-SwiftUI
+//
+//  Created by Serhii Berehovyi on 29.11.22.
+//
+
+import SwiftUI
+
+@main
+struct WordGarden_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
