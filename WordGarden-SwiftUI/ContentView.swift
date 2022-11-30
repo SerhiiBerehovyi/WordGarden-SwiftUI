586769c07b615e85bd4c299dd19adeb760527571
serhii.berehovyi <serhii.berehovyi@gmail.com>
2022-11-30 21:05:10 +0100

    Interface Created

--- WordGarden-SwiftUI/ContentView.swift
+++ WordGarden-SwiftUI/ContentView.swift
@@ -8,14 +8,81 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var wordsGuessed = 0
+    @State private var wordsMissed = 0
+    
+    @State private var wordsInGame = ["DOG", "CAT", "SWIFT"]
+    
+    @State private var gameStatusMessage = "How Many Guesses To Uncover the Hidden Word?"
+    @State private var guessedLetter = ""
+    @State private var imageName = "flower8"
+    
+    @State private var playAgainHidden = true
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack{
+                VStack (alignment: .leading) {
+                    Text("Words Guessed: \(wordsGuessed)")
+                    Text("Words Missed: \(wordsMissed)")
+                }
+                
+                Spacer()
+                
+                VStack (alignment: .trailing) {
+                    Text("Words to Guess: \(wordsInGame.count - (wordsMissed + wordsGuessed))")
+                    Text("Words in Game: \(wordsInGame.count)")
+                }
+            }
+            
+            Spacer()
+            
+            Text(gameStatusMessage)
+                .font(.title)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            Spacer()
+            
+            // TODO: Switch to wordsToGuess[currentWord]
+            Text("_ _ _ _ _")
+                .font(.title)
+            
+            if playAgainHidden {
+                HStack {
+                    TextField("", text: $guessedLetter)
+                        .textFieldStyle(.roundedBorder)
+                        .frame(width: 30)
+                        .overlay{
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(.gray, lineWidth: 2)
+                        }
+                    
+                    Button("Guess a Letter", action: {
+                        //TODO: Guess a Letter Button action here
+                    })
+                    .buttonStyle(.bordered)
+                    .tint(.mint)
+                }
+            } else {
+                Button("Another Word?"){
+                    //TODO: Another Word Button action here
+                    playAgainHidden = true
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.mint)
+            }
+            
+            Spacer()
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                
         }
-        .padding()
+        .padding(.horizontal)
+        .ignoresSafeArea(edges: .bottom)
     }
 }
 
